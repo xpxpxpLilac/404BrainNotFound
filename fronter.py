d9d84f159148a7a56355c98d8904df1e48b7ad5b
lianshuaisong <43778492+lianshuaisong@users.noreply.github.com>
2018-10-18 20:25:43 +1100

    Update fronter.py

--- fronter.py
+++ fronter.py
@@ -36,8 +36,7 @@ def signin():
 
 @app.route('/predictions', methods=['GET'])
 def get_pred_list():
-
-    response = requests.get('http://127.0.0.1:5000/predictions', headers={'AUTH-TOKEN':token} )
+    response = requests.get('http://127.0.0.1:5000/predictions', headers={'AUTH-TOKEN':token})
     print(token)
     item = response.json()
     print(item)
@@ -47,48 +46,51 @@ def get_pred_list():
 
 @app.route('/predictions', methods=['POST'])
 def post_pred():
-    team1 = request.form['team1']
-    team2 = request.form['team2']
-    team1speed = request.form['team1speed']
-    team2speed = request.form['team2speed']
-    team1crossing = request.form['team1crossing']
-    team2crossing = request.form['team2crossing']
-    global pid
-    data = {'prediction_id':pid,
-    'team1':team1,
-    'team2':team2,
-    'team1speed':int(team1speed),
-    'team2speed':int(team2speed),
-    'team1crossing':int(team1crossing),
-    'team2crossing':int(team2crossing)
-    }
-    #
-    #'Content-Type': 'application/json'
+	if 'p_id' in request.form:
+		p_id = request.form['p_id']
+		response = requests.get('http://127.0.0.1:5000/predictions/'+str(p_id), headers={'AUTH-TOKEN':token} )
+		item = response.json()
+		print(item)
+		if response.ok:
+			return render_template('signin-ok.html', username='username',data3= item)
+		return render_template('signin-ok.html', username='username',data3=item['message'])
 
-    print(data)
+	if 'team1' in request.form:
+		team1 = request.form['team1']
+		team2 = request.form['team2']
+		team1speed = request.form['team1speed']
+		team2speed = request.form['team2speed']
+		team1crossing = request.form['team1crossing']
+		team2crossing = request.form['team2crossing']
+		global pid
+		data = {'prediction_id':pid,
+		'team1':team1,
+		'team2':team2,
+		'team1speed':int(team1speed),
+		'team2speed':int(team2speed),
+		'team1crossing':int(team1crossing),
+		'team2crossing':int(team2crossing)
+		}
+	    #
+	    #'Content-Type': 'application/json'
 
-    response = requests.post('http://127.0.0.1:5000/predictions',headers={'AUTH-TOKEN':token},json=data)
-    item = response.json()
-    print(item)
-    if response.ok:
-    	return render_template('signin-ok.html', username='username',data2=item)
-    return render_template('signin-ok.html', username='username',data2=item['message'])
+		print(data)
 
+		response = requests.post('http://127.0.0.1:5000/predictions',headers={'AUTH-TOKEN':token},json=data)
+		item = response.json()
+		print(item)
+		if response.ok:
+			return render_template('signin-ok.html', username='username',data2=item)
+		return render_template('signin-ok.html', username='username',data2=item['message'])
 
-@app.route('/predictions/<p_id>', methods=['GET'])
-def get_pred(p_id):
-    #preid = request.form['preid']
-    print(p_id)
-
-    response = requests.get('http://127.0.0.1:5000/predictions/'+str(p_id), headers={'AUTH-TOKEN':token} )
-    print(token)
-    item = response.json()
-    print(item)
-    if response.ok:
-    	return render_template('signin-ok.html', username='username',data3= item)
-    return render_template('signin-ok.html', username='username',data3=item['message'])
-
-
+	if 'delete_id' in request.form:
+		delete_id = request.form['delete_id']
+		response = requests.delete('http://127.0.0.1:5000/predictions/'+str(delete_id), headers={'AUTH-TOKEN':token} )
+		item = response.json()
+		print(item)
+		if response.ok:
+			return render_template('signin-ok.html', username='username',data4= item)
+		return render_template('signin-ok.html', username='username',data4=item['message'])
 
 
 if __name__ == '__main__':
