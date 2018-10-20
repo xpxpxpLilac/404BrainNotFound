98f22e292a52d18d45197a009175343eed2fbef2
G7766 <37474714+G7766@users.noreply.github.com>
2018-10-21 00:46:19 +1100

    Add files via upload

--- service.py
+++ service.py
@@ -13,6 +13,9 @@ from flask_restplus import reqparse
 from pymongo import *
 import datetime
 from itsdangerous import SignatureExpired, JSONWebSignatureSerializer, BadSignature
+import ml
+import sqlite3
+import numpy as np
 
 class AuthenticationToken:
     def __init__(self, secret_key, expires_in):
@@ -77,15 +80,27 @@ def requires_auth(f):
 
 
 # The following is the schema of match
-match_model = api.model('match', {
-    'prediction_id': fields.Integer,
-    'team1': fields.String,
-    'team2': fields.String,
-    'team1speed': fields.Integer,
-    'team2speed': fields.Integer,
-    'team1crossing': fields.Integer,
-    'team2crossing': fields.Integer,
-})
+match_model = api.model('match', {'name':fields.String,
+                'H_buildUpPlaySpeed':fields.Integer,
+                'H_buildUpPlayDribbling':fields.Integer,
+                'H_buildUpPlayPassing':fields.Integer,
+                'H_chanceCreationPassing':fields.Integer,
+                'H_chanceCreationCrossing':fields.Integer,
+                'H_chanceCreationShooting':fields.Integer,
+                'H_defencePressure':fields.Integer,
+                'H_defenceAggression':fields.Integer,
+                'H_defenceTeamWidth':fields.Integer,
+
+                'A_buildUpPlayDribbling':fields.Integer,
+                'A_buildUpPlaySpeed':fields.Integer,
+                'A_buildUpPlayPassing':fields.Integer,
+                'A_chanceCreationPassing':fields.Integer,
+                'A_chanceCreationCrossing':fields.Integer,
+                'A_chanceCreationShooting':fields.Integer,
+                'A_defencePressure':fields.Integer,
+                'A_defenceAggression':fields.Integer,
+                'A_defenceTeamWidth':fields.Integer
+                })
 
 credential_model = api.model('credential', {
     'username': fields.String,
@@ -107,13 +122,36 @@ class Token(Resource):
 
         username = args.get('username')
         password = args.get('password')
+        user = db.users.find_one({"username": username})
+        print('=============')
+        if not user:
+            api.abort(404, "Username: {} doesn't exist".format(username))
+        if password != user['password']:
+            api.abort(401, "Wrong password")
+        return {"token": auth.generate_token(username)}
 
-        if username == 'admin' and password == 'admin':
-            return {"token": auth.generate_token(username)}
 
-        return {"message": "authorization has been refused for those credentials."}, 401
+    @api.response(200, 'Successful')
+    @api.doc(description="Generates a authentication token")
+    @api.expect(credential_parser, validate=True)
+    def post(self):
+        args = credential_parser.parse_args()
 
+        username = args.get('username')
+        password = args.get('password')
+        user = {'username':username,
+                'password': password
+        }
 
+        posts = db.users
+        try:
+            posts.insert_one(user)
+        except:
+            return {"message" : "{} has already been signed".format(username)}, 400
+        return { 
+            "message" : "{} Register Successfully".format(username), 
+            "prediction_id" : username
+            }, 200
 
 
 @api.route('/predictions')
@@ -126,50 +164,69 @@ class PredList(Resource):
     @requires_auth
     def post(self):
         match = request.json
+        match_id = match['name']
+        match_input = np.array([(match['H_buildUpPlaySpeed'],match['H_buildUpPlayDribbling'],match['H_buildUpPlayPassing'],\
+            match['H_chanceCreationPassing'],match['H_chanceCreationCrossing'],match['H_chanceCreationShooting'],\
+            match['H_defencePressure'],match['H_defenceAggression'],match['H_defenceTeamWidth'],\
+            match['A_buildUpPlayDribbling'],match['A_buildUpPlaySpeed'],match['A_buildUpPlayPassing'],\
+            match['A_chanceCreationPassing'],match['A_chanceCreationCrossing'],match['A_chanceCreationShooting'],\
+            match['A_defencePressure'],match['A_defenceAggression'],match['A_defenceTeamWidth'])])
         print(f'get post request: {match}')
-#############################################
-######## ML: give match_model, get prediction
-#############################################
-        prediction = {
-        '_id': 3,
-        'team1':'team1_name',
-        'team2':'team2_name',
-        'team1speed':60,
-        'team2speed':71,
-        'team1crossing':39,
-        'team2crossing':45,
-        'pred':'0:0'
-        }
-
+        p = ml.regression_prediction(regre, match_X_test, match_y_test, match_input, match_id)
+        print(f'get pred:\n{p}')
+        
         time = datetime.datetime.now()
         date_str = time.strftime("%Y-%m-%d %H:%M:%S")
-        posts = db.posts        
-        prediction_id = prediction['_id']
+        posts = db.posts
+        print(type(p))
+        prediction = {'_id':match['name'],
+                'H_buildUpPlaySpeed':match['H_buildUpPlaySpeed'],
+                'H_buildUpPlayDribbling':match['H_buildUpPlayDribbling'],
+                'H_buildUpPlayPassing':match['H_buildUpPlayPassing'],
+                'H_chanceCreationPassing':match['H_chanceCreationPassing'],
+                'H_chanceCreationCrossing':match['H_chanceCreationCrossing'],
+                'H_chanceCreationShooting':match['H_chanceCreationShooting'],
+                'H_defencePressure':match['H_defencePressure'],
+                'H_defenceAggression':match['H_defenceAggression'],
+                'H_defenceTeamWidth':match['H_defenceTeamWidth'],
+
+                'A_buildUpPlayDribbling':match['A_buildUpPlayDribbling'],
+                'A_buildUpPlaySpeed':match['A_buildUpPlaySpeed'],
+                'A_buildUpPlayPassing':match['A_buildUpPlayPassing'],
+                'A_chanceCreationPassing':match['A_chanceCreationPassing'],
+                'A_chanceCreationCrossing':match['A_chanceCreationCrossing'],
+                'A_chanceCreationShooting':match['A_chanceCreationShooting'],
+                'A_defencePressure':match['A_defencePressure'],
+                'A_defenceAggression':match['A_defenceAggression'],
+                'A_defenceTeamWidth':match['A_defenceTeamWidth'],
+
+                'result':p['prediction'],
+                'accuracy':p['accuracy']
+                }
         try:
             posts.insert_one(prediction)
         except:
-            return {"message" : "{} has already been posted".format(prediction_id),
-                    "location" : "/posts/{}".format(prediction_id)}, 200
+            print('aaaaaaaa')
+            return {"message" : "{} has already been posted".format(match_id),
+                    "location" : "/posts/{}".format(match_id)}, 200
         return { 
-            "location" : "/predictions/{}".format(prediction_id), 
-            "prediction_id" : prediction_id,  
+            "location" : "/predictions/{}".format(match_id), 
+            "prediction_id" : match_id,  
             "creation_time": date_str
             }, 201
 
 
 
     @api.response(200, 'Successful')
-    @api.doc(description="Get all books")
+    @api.doc(description="Get all pred")
     @requires_auth
     def get(self):
         availiable_pred = []
         for item in db.posts.find():      
             p_id = item["_id"] 
             pred = { 
-                "location" : "/posts/{}".format(p_id), 
-                "prediction_id" : p_id,
-                "team1": item['team1'],
-                "team2": item['team2']
+                "location" : "/predictions/{}".format(p_id), 
+                "prediction_id" : p_id
                 }  
             availiable_pred.append(pred)
         return availiable_pred, 200
@@ -182,12 +239,12 @@ class PredList(Resource):
 class Pred(Resource):
     @api.response(404, 'prediction was not found')
     @api.response(200, 'Successful')
-    @api.doc(description="Get a prediction by its ID")
+    @api.doc(description="Get a detailed prediction by its ID")
     @requires_auth
     def get(self, pid):
-        pred = db.posts.find_one({"_id": int(pid)})
+        pred = db.posts.find_one({"_id": pid})
         if not pred:
-            api.abort(404, "prediction with id {} doesn't exist".format(pid))
+            api.abort(404, "prediction with name: {} doesn't exist".format(pid))
         return pred, 200
 
 
@@ -196,7 +253,7 @@ class Pred(Resource):
     @api.doc(description="Delete a prediction by its ID")
     @requires_auth
     def delete(self, pid):
-        delete_id = db.posts.delete_one({"_id": int(pid)})
+        delete_id = db.posts.delete_one({"_id": pid})
         if delete_id.deleted_count:
             return { 
                 "message" :"prediction = {} is removed from the database!".format(pid)
@@ -205,6 +262,12 @@ class Pred(Resource):
         api.abort(404, "prediction: {} doesn't exist".format(pid))
 
 if __name__ == '__main__':
+    conn = sqlite3.connect("database.sqlite")
+
+    match = ml.getMatch(conn)
+    # Split the data into test and train parts
+    match_X_train, match_y_train, match_X_test, match_y_test = ml.load_match(match, split_percentage=0.7)
+    regre = ml.regression_train(match_X_train, match_y_train)
 
     client = MongoClient('mongodb://comp9321:comp9321@ds261332.mlab.com:61332/coralsdb')
     db = client['coralsdb']
